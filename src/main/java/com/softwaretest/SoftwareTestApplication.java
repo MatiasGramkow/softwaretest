de6daea5227e006faaa3dd47da741c9de9dba923
Matias Moeskjær Gramkow <mati2441@stud.kea.dk>
2020-05-11 10:23:10 +0200

    Initial commit

---
+++ src/main/java/com/softwaretest/SoftwareTestApplication.java
@@ -0,0 +1,14 @@
+package com.softwaretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareTestApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SoftwareTestApplication.class, args);
+    }
+}
---
+++ src/test/java/com/softwaretest/SoftwareTestApplicationTests.java
@@ -0,0 +1,15 @@
+package com.softwaretest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareTestApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
