ce95d267721affbdca1fd7064059229fb7a565ec
Radoslav Naydenov <radoales@users.noreply.github.com>
2020-05-21 14:20:31 +0200

    Add AdminController GetUsers Test

--- src/test/java/com/softwaretest/Controllers/AdminControllerTests.java
+++ src/test/java/com/softwaretest/Controllers/AdminControllerTests.java
@@ -1,6 +1,9 @@
 package com.softwaretest.Controllers;
 
+import com.softwaretest.Services.UserService.IUserService;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import org.springframework.ui.Model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,8 +11,17 @@ class AdminControllerTests
 {
 
     @Test
-    void getUsers()
+    void getUsers_ShouldReturn_HTMLNameForGetUsers()
     {
+        // Given
+        AdminController adminController = new AdminController();
+        Model model = mock(Model.class);
+
+        // When
+       String result = adminController.getUsers(model);
+
+        // Then
+        assertEquals("admin/userList", result);
     }
 
     @Test
