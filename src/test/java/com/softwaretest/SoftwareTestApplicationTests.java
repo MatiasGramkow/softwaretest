680308aa0cb7e2cb214c6a00aa87bf94e497edef
NielsJustesen <niels449@hotmail.com>
2020-05-21 09:53:38 +0200

    testing CI with two tests

--- src/test/java/com/softwaretest/SoftwareTestApplicationTests.java
+++ src/test/java/com/softwaretest/SoftwareTestApplicationTests.java
@@ -2,14 +2,25 @@ package com.softwaretest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class SoftwareTestApplicationTests
 {
 
     @Test
-    void contextLoads()
+    void trueTest()
     {
+
+        Assert.isTrue(true);
+    }
+
+
+    @Test
+    void falseTest()
+    {
+
+        Assert.isTrue(false);
     }
 
 }
