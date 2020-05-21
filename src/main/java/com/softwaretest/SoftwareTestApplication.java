581b43da980e6c7c1030a7ac0619550827d34886
Matias Moeskjær Gramkow <mati2441@stud.kea.dk>
2020-05-21 09:31:10 +0200

    Windows added

--- src/main/java/com/softwaretest/SoftwareTestApplication.java
+++ src/main/java/com/softwaretest/SoftwareTestApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SoftwareTestApplication
 {
-
     public static void main(String[] args)
     {
         SpringApplication.run(SoftwareTestApplication.class, args);
