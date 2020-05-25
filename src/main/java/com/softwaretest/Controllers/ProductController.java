ebdb7e08a123698e870bf9e545dc657ed4c3d3f7
Radoslav Naydenov <radoales@users.noreply.github.com>
2020-05-25 15:51:49 +0200

    Add html pages

--- src/main/java/com/softwaretest/Controllers/ProductController.java
+++ src/main/java/com/softwaretest/Controllers/ProductController.java
@@ -1,15 +1,11 @@
 package com.softwaretest.Controllers;
 
 import com.softwaretest.Models.Product;
-import com.softwaretest.Models.User;
 import com.softwaretest.Services.ProductService.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
