bdd42ed77fc54d34f985b15540b47d2f87df1539
NielsJustesen <niels449@hotmail.com>
2020-05-27 12:09:06 +0200

    Add Dataprovider for Signup Selenium Unit Tests

--- src/main/java/selenium/SeleniumSignUpTests.java
+++ src/main/java/selenium/SeleniumSignUpTests.java
@@ -4,8 +4,12 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+
+import java.sql.Driver;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.Assert.assertEquals;
@@ -24,34 +28,10 @@ public class SeleniumSignUpTests {
     }
 
     @AfterClass
-    public static void tearDown() {
+    public static void tearDown()
+    {
 
     }
-
-    @Test
-    public void whenUsernameProvidedOnSignUpIsTooShort_thenErrorMessageRecieved(){
-
-        String expected = "Username too short";
-
-        config.getDriver().get(url);
-        assertEquals("Create User", config.getDriver().getTitle());
-
-        WebElement elementEmail = config.getDriver().findElement(By.id("email"));
-        WebElement elementUsername = config.getDriver().findElement(By.id("username"));
-        WebElement elementPassword = config.getDriver().findElement(By.id("password"));
-        WebElement elementConfirmPassword = config.getDriver().findElement(By.id("retypePassword"));
-        WebElement elementSubmit = config.getDriver().findElement(By.id("submit"));
-
-        elementEmail.sendKeys("test@test.dk");
-        elementUsername.sendKeys("ttt");
-        elementPassword.sendKeys("thispassword");
-        elementConfirmPassword.sendKeys("thispassword");
-        elementSubmit.click();
-        String error = config.getDriver().findElement(By.id("username-error")).getText();
-        assertEquals(expected,error);
-    }
-
-
 
     @Test
     public void T12_whenUserIs_Correctly_SignedUp_thenUserCanLogIn() {
@@ -78,13 +58,10 @@ public class SeleniumSignUpTests {
         config.getDriver().close();
     }
 
-    @Test
-    public void T13_whenEmailIs_Invalid_OnSignUp_thenErrorMessageIsShown()
+    @ParameterizedTest(name = "testCase={0}, email={1}, username{2}, password={3}, retypePassword{4}, elementID={5}, expected={6}")
+    @CsvFileSource(resources = "/SeleniumData/SignUpCredentials.csv", numLinesToSkip = 1)
+    public void signUp_WithInvalidCredentials_ShouldReturnErrorMesssage(String testCase, String email, String username, String password, String retypePassword, String elementID, String expected)
     {
-        String email = "email";
-        String username = "testusername";
-        String testPassword = "testpassword";
-
         config.getDriver().get(url);
 
         WebElement emailElement = config.getDriver().findElement(By.id("email"));
@@ -97,392 +74,22 @@ public class SeleniumSignUpTests {
 
         WebElement passwordElement = config.getDriver().findElement(By.id("password"));
         passwordElement.clear();
-        passwordElement.sendKeys(testPassword);
+        passwordElement.sendKeys(password);
 
         WebElement retypePasswordElement = config.getDriver().findElement(By.id("retypePassword"));
         retypePasswordElement.clear();
-        retypePasswordElement.sendKeys(testPassword);
+        retypePasswordElement.sendKeys(retypePassword);
 
         WebElement updateBtn = config.getDriver().findElement(By.id("submit"));
         updateBtn.click();
 
-        String response = config.getDriver().findElement(By.id("email-error")).getText();
-        assertEquals(response, "must be a well-formed email address");
 
-        config.getDriver().close();
-    }
+        WebElement header1 = config.getDriver().findElement(By.tagName("h1"));
+        System.out.println(header1.toString());
 
-    @Test
-    public void T14_whenEmailFieldIs_Empty_OnSignUp_thenErrorMessageIsShown()
-    {
-        String email = "";
-        String username = "testusername";
-        String testPassword = "testpassword";
+        String response = config.getDriver().findElement(By.id(elementID)).getText();
+        assertEquals(expected, response);
 
-        config.getDriver().get(url);
-
-        WebElement emailElement = config.getDriver().findElement(By.id("email"));
-        emailElement.clear();
-        emailElement.sendKeys(email);
-
-        WebElement usernameElement = config.getDriver().findElement(By.id("username"));
-        usernameElement.clear();
-        usernameElement.sendKeys(username);
-
-        WebElement passwordElement = config.getDriver().findElement(By.id("password"));
-        passwordElement.clear();
-        passwordElement.sendKeys(testPassword);
-
-        WebElement retypePasswordElement = config.getDriver().findElement(By.id("retypePassword"));
-        retypePasswordElement.clear();
-        retypePasswordElement.sendKeys(testPassword);
-
-        WebElement updateBtn = config.getDriver().findElement(By.id("submit"));
-        updateBtn.click();
-
-        String response = config.getDriver().findElement(By.id("email-error")).getText();
-        assertEquals(response, "email missing");
-
-        config.getDriver().close();
-    }
-
-    @Test
-    public void T15_whenEmailFieldIs_TooLong_OnSignUp_thenErrorMessageIsShown()
-    {
-        String email = "ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss@s.dk";
-        String username = "testusername";
-        String testPassword = "testpassword";
-
-        config.getDriver().get(url);
-
-        WebElement emailElement = config.getDriver().findElement(By.id("email"));
-        emailElement.clear();
-        emailElement.sendKeys(email);
-
-        WebElement usernameElement = config.getDriver().findElement(By.id("username"));
-        usernameElement.clear();
-        usernameElement.sendKeys(username);
-
-        WebElement passwordElement = config.getDriver().findElement(By.id("password"));
-        passwordElement.clear();
-        passwordElement.sendKeys(testPassword);
-
-        WebElement retypePasswordElement = config.getDriver().findElement(By.id("retypePassword"));
-        retypePasswordElement.clear();
-        retypePasswordElement.sendKeys(testPassword);
-
-        WebElement updateBtn = config.getDriver().findElement(By.id("submit"));
-        updateBtn.click();
-
-        String response = config.getDriver().findElement(By.id("email-error")).getText();
-        assertEquals(response, "Email too long");
-
-        config.getDriver().close();
-    }
-
-
-    @Test
-    public void T16_whenUsernameFieldIs_Empty_OnSignUp_thenErrorMessageIsShown()
-    {
-        String email = "email@gmail.com";
-        String username = "";
-        String testPassword = "testpassword";
-
-        config.getDriver().get(url);
-
-        WebElement emailElement = config.getDriver().findElement(By.id("email"));
-        emailElement.clear();
-        emailElement.sendKeys(email);
-
-        WebElement usernameElement = config.getDriver().findElement(By.id("username"));
-        usernameElement.clear();
-        usernameElement.sendKeys(username);
-
-        WebElement passwordElement = config.getDriver().findElement(By.id("password"));
-        passwordElement.clear();
-        passwordElement.sendKeys(testPassword);
-
-        WebElement retypePasswordElement = config.getDriver().findElement(By.id("retypePassword"));
-        retypePasswordElement.clear();
-        retypePasswordElement.sendKeys(testPassword);
-
-        WebElement updateBtn = config.getDriver().findElement(By.id("submit"));
-        updateBtn.click();
-
-        String response = config.getDriver().findElement(By.id("username-error")).getText();
-        assertEquals(response, "Required field");
-
-        config.getDriver().close();
-    }
-
-    @Test
-    public void T17_whenUsernameFieldIs_TooShort_OnSignUp_thenErrorMessageIsShown()
-    {
-        String email = "email@gmail.com";
-        String username = "test";
-        String testPassword = "testpassword";
-
-        config.getDriver().get(url);
-
-        WebElement emailElement = config.getDriver().findElement(By.id("email"));
-        emailElement.clear();
-        emailElement.sendKeys(email);
-
-        WebElement usernameElement = config.getDriver().findElement(By.id("username"));
-        usernameElement.clear();
-        usernameElement.sendKeys(username);
-
-        WebElement passwordElement = config.getDriver().findElement(By.id("password"));
-        passwordElement.clear();
-        passwordElement.sendKeys(testPassword);
-
-        WebElement retypePasswordElement = config.getDriver().findElement(By.id("retypePassword"));
-        retypePasswordElement.clear();
-        retypePasswordElement.sendKeys(testPassword);
-
-        WebElement updateBtn = config.getDriver().findElement(By.id("submit"));
-        updateBtn.click();
-
-        String response = config.getDriver().findElement(By.id("username-error")).getText();
-        assertEquals(response, "Username too short");
-
-        config.getDriver().close();
-    }
-
-    @Test
-    public void T18_whenUsernameFieldIs_TooLong_OnSignUp_thenErrorMessageIsShown()
-    {
-        String email = "email@gmail.com";
-        String username = "usernameisfartoolongtowork";
-        String testPassword = "testpassword";
-
-        config.getDriver().get(url);
-
-        WebElement emailElement = config.getDriver().findElement(By.id("email"));
-        emailElement.clear();
-        emailElement.sendKeys(email);
-
-        WebElement usernameElement = config.getDriver().findElement(By.id("username"));
-        usernameElement.clear();
-        usernameElement.sendKeys(username);
-
-        WebElement passwordElement = config.getDriver().findElement(By.id("password"));
-        passwordElement.clear();
-        passwordElement.sendKeys(testPassword);
-
-        WebElement retypePasswordElement = config.getDriver().findElement(By.id("retypePassword"));
-        retypePasswordElement.clear();
-        retypePasswordElement.sendKeys(testPassword);
-
-        WebElement updateBtn = config.getDriver().findElement(By.id("submit"));
-        updateBtn.click();
-
-        String response = config.getDriver().findElement(By.id("username-error")).getText();
-        assertEquals(response, "Username too long");
-
-        config.getDriver().close();
-    }
-
-    @Test
-    public void T19_whenUsernameFieldContains_Symbols_OnSignUp_thenErrorMessageIsShown()
-    {
-        String email = "email@gmail.com";
-        String username = "&&!)(¤";
-        String testPassword = "testpassword";
-
-        config.getDriver().get(url);
-
-        WebElement emailElement = config.getDriver().findElement(By.id("email"));
-        emailElement.clear();
-        emailElement.sendKeys(email);
-
-        WebElement usernameElement = config.getDriver().findElement(By.id("username"));
-        usernameElement.clear();
-        usernameElement.sendKeys(username);
-
-        WebElement passwordElement = config.getDriver().findElement(By.id("password"));
-        passwordElement.clear();
-        passwordElement.sendKeys(testPassword);
-
-        WebElement retypePasswordElement = config.getDriver().findElement(By.id("retypePassword"));
-        retypePasswordElement.clear();
-        retypePasswordElement.sendKeys(testPassword);
-
-        WebElement updateBtn = config.getDriver().findElement(By.id("submit"));
-        updateBtn.click();
-
-        String response = config.getDriver().findElement(By.id("username-error")).getText();
-        assertEquals(response, "Invalid username");
-
-        config.getDriver().close();
-    }
-
-    @Test
-    public void T20_whenUsernameFieldContains_Spaces_OnSignUp_thenErrorMessageIsShown()
-    {
-        String email = "email@gmail.com";
-        String username = "Ole Nielsen";
-        String testPassword = "testpassword";
-
-        config.getDriver().get(url);
-
-        WebElement emailElement = config.getDriver().findElement(By.id("email"));
-        emailElement.clear();
-        emailElement.sendKeys(email);
-
-        WebElement usernameElement = config.getDriver().findElement(By.id("username"));
-        usernameElement.clear();
-        usernameElement.sendKeys(username);
-
-        WebElement passwordElement = config.getDriver().findElement(By.id("password"));
-        passwordElement.clear();
-        passwordElement.sendKeys(testPassword);
-
-        WebElement retypePasswordElement = config.getDriver().findElement(By.id("retypePassword"));
-        retypePasswordElement.clear();
-        retypePasswordElement.sendKeys(testPassword);
-
-        WebElement updateBtn = config.getDriver().findElement(By.id("submit"));
-        updateBtn.click();
-
-        String response = config.getDriver().findElement(By.id("username-error")).getText();
-        assertEquals(response, "Invalid username");
-
-        config.getDriver().close();
-    }
-
-    @Test
-    public void T21_whenPasswordProvidedOnSignUpIs_TooShort_thenErrorMessageRecieved()
-    {
-        String email = "email@gmail.com";
-        String username = "testusername";
-        String testPassword = "1234567";
-
-        config.getDriver().get(url);
-
-        WebElement emailElement = config.getDriver().findElement(By.id("email"));
-        emailElement.clear();
-        emailElement.sendKeys(email);
-
-        WebElement usernameElement = config.getDriver().findElement(By.id("username"));
-        usernameElement.clear();
-        usernameElement.sendKeys(username);
-
-        WebElement passwordElement = config.getDriver().findElement(By.id("password"));
-        passwordElement.clear();
-        passwordElement.sendKeys(testPassword);
-
-        WebElement retypePasswordElement = config.getDriver().findElement(By.id("retypePassword"));
-        retypePasswordElement.clear();
-        retypePasswordElement.sendKeys(testPassword);
-
-        WebElement updateBtn = config.getDriver().findElement(By.id("submit"));
-        updateBtn.click();
-
-        String response = config.getDriver().findElement(By.id("password-error")).getText();
-        assertEquals(response, "Password too short");
-
-        config.getDriver().close();
-
-    }
-
-    @Test
-    public void T22_whenPasswordProvidedOnSignUpIs_TooLong_thenErrorMessageRecieved(){
-        String email = "email@gmail.com";
-        String username = "testusername";
-        String testPassword = "abcdefghilkjqsaws";
-
-        config.getDriver().get(url);
-
-        WebElement emailElement = config.getDriver().findElement(By.id("email"));
-        emailElement.clear();
-        emailElement.sendKeys(email);
-
-        WebElement usernameElement = config.getDriver().findElement(By.id("username"));
-        usernameElement.clear();
-        usernameElement.sendKeys(username);
-
-        WebElement passwordElement = config.getDriver().findElement(By.id("password"));
-        passwordElement.clear();
-        passwordElement.sendKeys(testPassword);
-
-        WebElement retypePasswordElement = config.getDriver().findElement(By.id("retypePassword"));
-        retypePasswordElement.clear();
-        retypePasswordElement.sendKeys(testPassword);
-
-        WebElement updateBtn = config.getDriver().findElement(By.id("submit"));
-        updateBtn.click();
-
-        String response = config.getDriver().findElement(By.id("password-error")).getText();
-        assertEquals(response, "Password too long");
-
-        config.getDriver().close();
-    }
-
-    @Test
-    public void T23_whenPasswordProvidedOnSignUpIs_Missing_thenErrorMessageRecieved(){
-        String email = "email@gmail.com";
-        String username = "testusername";
-        String testPassword = "";
-
-        config.getDriver().get(url);
-
-        WebElement emailElement = config.getDriver().findElement(By.id("email"));
-        emailElement.clear();
-        emailElement.sendKeys(email);
-
-        WebElement usernameElement = config.getDriver().findElement(By.id("username"));
-        usernameElement.clear();
-        usernameElement.sendKeys(username);
-
-        WebElement passwordElement = config.getDriver().findElement(By.id("password"));
-        passwordElement.clear();
-        passwordElement.sendKeys(testPassword);
-
-        WebElement retypePasswordElement = config.getDriver().findElement(By.id("retypePassword"));
-        retypePasswordElement.clear();
-        retypePasswordElement.sendKeys(testPassword);
-
-        WebElement updateBtn = config.getDriver().findElement(By.id("submit"));
-        updateBtn.click();
-
-        String response = config.getDriver().findElement(By.id("password-error")).getText();
-        assertEquals(response, "Required field");
-
-        config.getDriver().close();
-    }
-
-    @Test
-    public void T24_whenPasswordAndConfirmPasswordDoNotMatch_thenErrorMessageRecieved(){
-        String email = "email@gmail.com";
-        String username = "testusername";
-        String testPassword = "testpassword";
-        String confirmPassword = "incorrectpass";
-
-        config.getDriver().get(url);
-
-        WebElement emailElement = config.getDriver().findElement(By.id("email"));
-        emailElement.clear();
-        emailElement.sendKeys(email);
-
-        WebElement usernameElement = config.getDriver().findElement(By.id("username"));
-        usernameElement.clear();
-        usernameElement.sendKeys(username);
-
-        WebElement passwordElement = config.getDriver().findElement(By.id("password"));
-        passwordElement.clear();
-        passwordElement.sendKeys(testPassword);
-
-        WebElement retypePasswordElement = config.getDriver().findElement(By.id("retypePassword"));
-        retypePasswordElement.clear();
-        retypePasswordElement.sendKeys(confirmPassword);
-
-        WebElement updateBtn = config.getDriver().findElement(By.id("submit"));
-        updateBtn.click();
-
-        String response = config.getDriver().findElement(By.id("password-error")).getText();
-        assertEquals(response, "Passwords do not match");
-
-        config.getDriver().close();
+        config.getDriver().quit();
     }
 }
